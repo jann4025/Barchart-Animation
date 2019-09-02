d9f3f7ac8a784967fa40f4ed4ea8486cb9afaa8c
jannick holm <jannickholm@jannicks-MacBook-Pro.local>
2019-09-02 15:54:22 +0200

    Solution

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict'
+document.addEventListener('DOMContentLoaded', start);
+
+function start() {
+    console.log('Connection between the DOM and the Script was successfull! Nice 😎');
+    let number = Math.floor(Math.random() * 36);
+    const array = ["20", "10", "24", "25", "32", "34", "21", "20", "10", "24", "25", "32", "34", "21", "20", "10", "24", "25", "32", "34", "21", "20", "10", "24", "25", "32", "34", "21", "10", "24", "25", "32", "34", "21", "20", "10", "24", "25", "32", "34"];
+    document.querySelectorAll(".bar").forEach((bar, i) => {
+        bar.style.height = array[i] + "vh";
+    });
+    setInterval(function () {
+        number = Math.floor(Math.random() * 36);
+        array.push(number);
+        console.clear();
+        console.log(array);
+        if (array.length > 40) {
+            array.shift();
+        }
+        document.querySelectorAll(".bar").forEach((bar, i) => {
+            bar.style.height = array[i] + "vh";
+        });
+
+    }, 300);
+
+
+
+
+}
