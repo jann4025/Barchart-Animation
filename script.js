c7c9494cdc97d6b8eec0c66eddaf707ce6abff2f
jannick holm <jannickholm@laptop-3m161nbj.kea.local>
2019-09-03 08:48:57 +0200

    ..

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ document.addEventListener('DOMContentLoaded', start);
 
 function start() {
     console.log('Connection between the DOM and the Script was successfull! Nice 😎');
-    let number;
+    let RandomNumber;
     const array = ["20", "10", "24", "25", "32", "34", "21", "20", "10", "24", "25", "32", "34", "21", "20", "10", "24", "25", "32", "34", "21", "20", "10", "24", "25", "32", "34", "21", "10", "24", "25", "32", "34", "21", "20", "10", "24", "25", "32", "34"];
     document.querySelectorAll(".bar").forEach((bar, i) => {
         bar.style.height = array[i] + "vh";
     });
     setInterval(function () {
-        number = Math.floor(Math.random() * 36);
-        array.push(number);
+        RandomNumber = Math.floor(Math.random() * 36);
+        array.push(RandomNumber);
         console.clear();
         console.log(array);
         if (array.length > 40) {
